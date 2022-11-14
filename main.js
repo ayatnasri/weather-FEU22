8a1f6b26b1119a2e1c66e618485403a9f626995b
Ayat Nasri <aayat.nasri@gmail.com>
2022-11-14 13:51:11 +0100

    Add some comments

--- main.js
+++ main.js
@@ -1,77 +1,65 @@
-let lat;
-let long;
-let country;
-let tempruture;
-let weatherDescription;
-let feelLike;
-let icon;
-let id;
-let APIKey = "f788b5fc412e548859e20d400c89a0b7";
+let APIKey = "f788b5fc412e548859e20d400c89a0b7";                        // Global variable
+                                                                        // (Async - AJAX) fetch. Call the server to get the
+                                                                        // latitude & longitude for the Helsingborg stad 
+let promise = fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Helsingborg&limit=1&appid=${APIKey}`)
+.then((respons) => {                                                    // send a request 
+    if(respons.ok) {
+        return respons.json();                                          // convert the response.text() to JSON document (Array)
+    }
+})
+.then(data => {                     
+    console.log(data);                                                  // get the response data from the server
+    country = `${data[0].name}, ${data[0].country}`;                    // get the country's name and the region's name 
+    lat = data[0].lat;                                                  // get the location latitude for Helsingborg stad 
+    long = data[0].lon;                                                 // get the location longitude for Helsingborg stad 
 
+                                                                        // fetch the URL to get the weather data
+                                                                        // units=metric To change the temperature from Kelvin to celsius   
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APIKey}&units=metric`) 
+        .then(respons => respons.json())                                // convert the response.text() to JSON (Object)
+        .then(data => {
+            console.log(data);                                          // get the response data from the server
+            let temperature = data.main.temp;                           // the temperature i Helsingborg now
+            let weatherDescription = data.weather[0].description;       // the weather discription 
+            let feelLike = data.main.feels_like;                       
+            // icon = data.weather[0].main;
 
-function getLocationData(){             // fetch the URL to get the latitude & longitude for the Helsingborg stad 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Helsingborg&limit=1&appid=${APIKey}`)
-    .then((respons) => {
-        if(respons.ok) {
-            return respons.json();
-        }
-    })
-    .then(data => {                     
-        console.log(data);
-        country = `${data[0].name}, ${data[0].country}`;
-        lat = data[0].lat;
-        long = data[0].lon;
+                                                                        // display the data on the screen - DOM
+            document.getElementById('region').innerHTML = 
+            `<img src='https://freesvg.org/storage/img/thumb/joede-location-symbol.png' class='locationIcon'/> ${country}` ;
+            document.getElementById('temp').innerHTML = `${Math.round(temperature)}<sup id="celsius">°C</sup>`;
+            document.getElementById('description').innerHTML = weatherDescription;
+            document.getElementById('feelLike').innerHTML = `Feel like : ${Math.round(feelLike)}<sup id="FeelLikeCelsius">°C</sup>`;
 
-        function getWeatherData(){      // fetch the URL to get the weather data   
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APIKey}&units=metric`) // units=metric To change the temperature from Kelvin to celsius
-            .then(respons => respons.json())
-            .then(data => {
-                console.log(data);
-                tempruture = data.main.temp;
-                weather = data.weather[0].description;
-                feelLike = data.main.feels_like;
-                // icon = data.weather[0].main;
-
-                                        // display the data on the screen - DOM
-                document.getElementById('region').innerHTML = `<img src='https://freesvg.org/storage/img/thumb/joede-location-symbol.png' class='locationIcon'/> ${country}` ;
-                document.getElementById('temp').innerHTML = `${Math.round(tempruture)}<sup id="celsius">°C</sup>`;
-                document.getElementById('description').innerHTML = weather;
-                document.getElementById('feelLike').innerHTML = `Feel like : ${Math.round(feelLike)}<sup id="FeelLikeCelsius">°C</sup>`;
-
-                // To get the icon, there are two methods:
-                // 1- The first method:  // get the icon from JSON document according to ('https://openweathermap.org/weather-conditions/')
+            // To get the icon, there are two methods:
+            // 1- get the icon from JSON document according to ('https://openweathermap.org/weather-conditions/')
                 /*
                 document.getElementById('icon').src = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
                 console.log(icon);
                 console.log(data.weather[0].icon);
                 */
 
-                // 2- The second method :  // Channge the weather icon according to the id ('https://openweathermap.org/weather-conditions/')
-                id = data.weather[0].id;
-                console.log(id);
-                icon = document.getElementById('icon');
+            // 2- Channge the weather icon according to the id ('https://openweathermap.org/weather-conditions/')
+            let id = data.weather[0].id;
+            let icon = document.getElementById('icon');
 
-                 if(id >= 200 && id <= 232){
-                    icon.src = 'https://freesvg.org/storage/img/thumb/weather-storm.png';
-                 } else if((id >= 300 && id <= 321) || (id >= 520 && id <= 531)){
-                    icon.src = 'https://cdn-icons-png.flaticon.com/128/8324/8324797.png';
-                 }else if(id >= 500 && id <= 504) {
-                    icon.src = 'https://cdn-icons-png.flaticon.com/128/8841/8841315.png';
-                 }else if(id == 511){
-                    icon.src = 'https://freesvg.org/storage/img/thumb/weather-snow.png';
-                 }else if(id >= 600 && id <= 622){
-                    icon.src = 'https://cdn-icons-png.flaticon.com/128/2315/2315377.png';
-                 }else if(id >= 701 && id <= 781){
-                    icon.src = 'https://freesvg.org/storage/img/thumb/Cartoon_Tornado.png';
-                 }else if(id == 800){
-                    icon.src = 'https://freesvg.org/storage/img/thumb/weather-clear.png';
-                 }else if(id >= 801 && id <= 804){
-                    icon.src = 'https://cdn-icons-png.flaticon.com/128/4064/4064276.png';
-                 }
-                console.log(icon);           
-            })
-        }
-        getWeatherData();               // Call the second function 
-    })
-}
-getLocationData()                       // Call the First function 
+            if(id >= 200 && id <= 232){
+                icon.src = 'https://freesvg.org/storage/img/thumb/weather-storm.png';      // weather description : thunderstorm
+            } else if((id >= 300 && id <= 321) || (id >= 520 && id <= 531)){
+                icon.src = 'https://cdn-icons-png.flaticon.com/128/8324/8324797.png';      // weather description : shower rain
+            }else if(id >= 500 && id <= 504) {
+                icon.src = 'https://cdn-icons-png.flaticon.com/128/8841/8841315.png';      // weather description :	rain
+            }else if(id == 511){
+                icon.src = 'https://freesvg.org/storage/img/thumb/weather-snow.png';       // weather description : freezing rain
+            }else if(id >= 600 && id <= 622){
+                icon.src = 'https://cdn-icons-png.flaticon.com/128/2315/2315377.png';      // weather description : Snow
+            }else if(id >= 701 && id <= 781){
+                icon.src = 'https://freesvg.org/storage/img/thumb/Cartoon_Tornado.png';    // weather description : strom
+            }else if(id == 800){
+                icon.src = 'https://freesvg.org/storage/img/thumb/weather-clear.png';      // weather description : sunny / clear
+            }else if(id >= 801 && id <= 804){
+                icon.src = 'https://cdn-icons-png.flaticon.com/128/4064/4064276.png';      // weather description : Clouds
+            }
+            console.log(icon);           
+        });     
+    });
